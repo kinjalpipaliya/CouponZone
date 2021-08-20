0f95b1a6d64c7bf495e55a0e44eb8d5f2d0ac03c
Kinjal Pipaliya <kinjalpipaliya88@gmail.com>
2021-08-20 23:53:17 +0530

    - Initial comment

---
+++ Couponzone/Couponzone/Utils/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  Couponzone
+//
+//  Created by Kinjal Pipaliya on 20/08/21.
+//
+
+import Foundation
---
+++ Couponzone/Couponzone/View/Cells/CollectionViewCells/OfferCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  OfferCollectionViewCell.swift
+//  Couponzone
+//
+//  Created by Kinjal Pipaliya on 20/08/21.
+//
+
+import UIKit
+
+class OfferCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
